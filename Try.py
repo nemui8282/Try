21cec594b0423d7cd1e7109aca601f3a89c1742a
nemui8282 <pazu.project@gmail.com>
2024-12-26 08:11:28 +0900

    Add files via upload

---
+++ Try.py
@@ -0,0 +1,12 @@
+#python 3.7.1
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Web App</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <button onclick="alert('Button Clicked!')">Click Me</button>
+</body>
+</html>
